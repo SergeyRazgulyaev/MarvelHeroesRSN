3a94687998f63316c1fbcded7b2b26d6b1d29085
SergeyRazgulyaev <razgulyaev.sergey@gmail.com>
2022-03-10 18:52:57 +0300

    NetworkServiceTests added

--- MarvelHeroesRSNTests/NetworkService/NetworkServiceTests.swift
+++ MarvelHeroesRSNTests/NetworkService/NetworkServiceTests.swift
@@ -9,13 +9,25 @@ import XCTest
 @testable import MarvelHeroesRSN
 
 class NetworkServiceTests: XCTestCase {
+	var sut: NetworkServiceProtocol!
+
+	let limit: Int = 50
+	let offset: Int = 0
+	var urlParametersContainer: URLParametersContainer!
 
     override func setUpWithError() throws {
+		urlParametersContainer = startURLParametersContainer
+		sut = NetworkService(urlParametersContainer: urlParametersContainer)
 
 	}
 
     override func tearDownWithError() throws {
-
+		urlParametersContainer = nil
+		sut = nil
 	}
 
+	func testIsDataLoading() {
+		sut.loadHeroesData(limit: limit, offset: offset) { _ in }
+		XCTAssertTrue(sut.isDataLoading)
+	}
 }
